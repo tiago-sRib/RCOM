332bf1352b3661087c3418fde397b5b9024340de
Tiago Ribeiro <61505694+tiago-sRib@users.noreply.github.com>
2022-06-01 15:54:05 +0100

    Working

--- Lab2/arg.c
+++ Lab2/arg.c
@@ -2,35 +2,68 @@
 #include "download.h"
 #include "macros.h"
 
-void parse_input(char * arg, requestedData * data){
+void parse_input(char * arg, requestedData * data)
+{
     char * token;
 
     if(!arg)
         return;
         
-
-    if(strncmp("ftp://", arg, 6) != 0){
+    if(strncmp("ftp://", arg, 6) != 0)
+    {
         printf("Argument must start with 'ftp://' \n");
         return;
     }
 
-    arg += 6;
+    arg += 6 * sizeof(char);
 
-    //printf("Url without 'ftp': %s\n", arg);
+    if ((strstr(arg, ":")==NULL) || (strstr(arg, "@") == NULL)) 
+    {
+        sprintf(data->user,"anonymous");
+        sprintf(data->password,"pass");
 
-    token = strtok(arg, ":");
-    strcpy(data->user, token);
-    token = strtok(NULL, "@");
-    strcpy(data->password, token);
-    token = strtok(NULL, "/");
-    strcpy(data->host, token);
+        token = strtok(arg, "/");
+        strncpy(data->host, token, MAX_STRING_SIZE);
+        
+    }
+    else
+    {
+        token = strtok(arg, ":");
+        strncpy(data->user, token, MAX_STRING_SIZE);
+        token = strtok(NULL, "@");
+        strncpy(data->password, token, MAX_STRING_SIZE);
+        token = strtok(NULL, "/");
+        strncpy(data->host, token, MAX_STRING_SIZE);
+    }
     token = strtok(NULL, "\0");
-    strcpy(data->url_path, token);
+    strncpy(data->url_path, token, MAX_STRING_SIZE);
+
+    char * aux_token = token;
+    int counter = 0;
+    while ((aux_token = strstr(aux_token, "/") ) != NULL)
+    {
+        counter++;
+        aux_token += sizeof(char);
+    }
+
+    if(counter > 0){
+        while ((token = strstr(token, "/") ) != NULL)
+        {
+            token += sizeof(char);
+            aux_token = token;    
+        }
+        strncpy(data->file_name, aux_token, MAX_STRING_SIZE);
+    }
+    else strncpy(data->file_name, data->url_path, MAX_STRING_SIZE);
 }
 
-void print_data_struct(requestedData * data){
-    printf("User: %s\n", data->user);
-    printf("Password: %s\n", data->password);
-    printf("Host: %s\n", data->host);
-    printf("Url: %s\n", data->url_path);
+
+void print_data_struct(requestedData * data)
+{
+    printf("Data inserted:\n");
+    printf("\tUser: %s\n"         , data->user);
+    printf("\tPassword: %s\n"     , data->password);
+    printf("\tHost: %s\n"         , data->host);
+    printf("\tUrl: %s\n"          , data->url_path);
+    printf("\tFile Name: %s\n\n"    , data->file_name);
 }
